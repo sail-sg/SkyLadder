ccf97862e639b5541221c79fdae52712152414e8
tongyao-zhu <312404930zhu@gmail.com>
2025-01-18 12:03:46 +0800

    Create constants.py

---
+++ lit_gpt/constants.py
@@ -0,0 +1,11 @@
+DM_ATTENTION_SUFFIX= ["dm1", "dm2", "dm4", "dm8", "dm1st4", "dm1st8", "dm1st16", "dm2st4", "dm2st8", "dm2st16",
+                            "dm4st4", "dm4st8", "dm4st16",
+                            "dm1st32", "dm2st32", "dm4st32", "dm8st32", "dm1st64", "dm2st64", "dm4st64", "dm8st64",
+                            "dm1st128", "dm2st128", "dm4st128", "dm8st128",
+                            "dm1st256", "dm2st256", "dm4st256", "dm8st256", "dm1st512", "dm2st512", "dm4st512",
+                            "dm8st512",
+                            ]
+
+INTRADM_ATTENTION_SUFFIX=['intradm1', 'intradm2', 'intradm4', 'intradm8']
+
+ALL_ATTENTION_SUFFIX = DM_ATTENTION_SUFFIX + INTRADM_ATTENTION_SUFFIX
